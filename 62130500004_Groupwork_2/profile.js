5c7cf2e51a6a427d534337ef33b979e2161d5d4d
Khorapinese <khorapin.pingadpu@mail.kmutt.ac.th>
2021-02-03 12:31:58 +0700

    pic

---
+++ 62130500004_Groupwork_2/profile.js
@@ -0,0 +1,19 @@
+    const app = {
+        data() {
+            return {
+                msg: 'Hello, Vue3',
+                name: 'Khorapin Gadpu',
+                occupation: 'Student',
+                article: 202,
+                followers: 835,
+                ratings: 9.9,
+                image: "images/S__16613428.jpg",
+                bg: "images/stardewbackground.png"
+
+            }
+        }
+            
+      
+                
+    }
+    Vue.createApp(app).mount('#app')
